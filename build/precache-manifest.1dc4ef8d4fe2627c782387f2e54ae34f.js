7a3981625f909ede7b55fbb3dba3d3eee5e388f3
KRAIG WILLIAMS <williamskrg@gmail.com>
2020-03-13 16:03:33 -0400

    start styling with font selection

---
+++ build/precache-manifest.1dc4ef8d4fe2627c782387f2e54ae34f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9cf68df900e3992341db4cca5c3d1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e151c4964fd3e5d101c",
+    "url": "/static/css/main.4cf08072.chunk.css"
+  },
+  {
+    "revision": "f0a05c62ff09fd81ce78",
+    "url": "/static/js/2.bb37c3cf.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.bb37c3cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e151c4964fd3e5d101c",
+    "url": "/static/js/main.2a0548a2.chunk.js"
+  },
+  {
+    "revision": "72c5965cbfb3ac6a55d7",
+    "url": "/static/js/runtime-main.40d8ffab.js"
+  },
+  {
+    "revision": "b93bb4960ecb848bfc0366963d42ceb7",
+    "url": "/static/media/searchpets.b93bb496.png"
+  }
+]);
