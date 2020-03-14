3ef908b3995281b4b55cdad72cf746569fa9fc0c
Bilbertius <lauchlinmacneil@gmail.com>
2020-03-13 19:59:17 -0600

    Fix API origin

---
+++ build/precache-manifest.e941af4be66886de56dd8f4b26ce9199.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742934fe60d2f7bf3d3386f7d4a38436",
+    "url": "/index.html"
+  },
+  {
+    "revision": "101b6ee210af3627672a",
+    "url": "/static/css/main.b076ebde.chunk.css"
+  },
+  {
+    "revision": "74aa7965858000d0e7e0",
+    "url": "/static/js/2.d34d79f6.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.d34d79f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "101b6ee210af3627672a",
+    "url": "/static/js/main.4fed0c1b.chunk.js"
+  },
+  {
+    "revision": "72c5965cbfb3ac6a55d7",
+    "url": "/static/js/runtime-main.40d8ffab.js"
+  },
+  {
+    "revision": "b93bb4960ecb848bfc0366963d42ceb7",
+    "url": "/static/media/searchpets.b93bb496.png"
+  }
+]);
--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-	API_BASE_URL: 'http://localhost:8080/api'
+	API_BASE_URL: 'http://petful-server.herokuapp.com/api'
 }
